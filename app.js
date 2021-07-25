ebda7ff5f96a853d1156a080769439be6ab9ee62
wilson614 <s110613053@stu.ntue.edu.tw>
2021-07-25 15:26:18 +0800

    Modify routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
@@ -7,10 +8,7 @@ const port = 3000
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`App is running on http://localhost:${port}`)
